b3835fc7d0dff3421313ea61fd099bf57ab9795e
heoh <h0h6h2h5@gmail.com>
2021-08-20 16:22:49 +0000

    주석 추가

--- heoh/programmers-42584.py
+++ heoh/programmers-42584.py
@@ -4,8 +4,8 @@ PRICE = 1
 def solution(prices):
     answer = [0] * len(prices)
 
-    stack = [(-1, 0)]
-    prices.append(0)
+    stack = [(-1, 0)]  # key error 방지
+    prices[-1] = 0  # 마지막 날 무조건 팔도록
     for i, price in enumerate(prices):
         while stack[-1][PRICE] > price:
             s = stack.pop()
@@ -14,3 +14,4 @@ def solution(prices):
         stack.append((i, price))
 
     return answer
+    
