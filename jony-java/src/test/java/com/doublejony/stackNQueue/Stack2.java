f16d3a7c21eeabd73d3dd76af4fe17134978ca3e
DoubleJONY <panzz8307@me.com>
2021-08-23 23:05:52 +0900

    문제 pull

--- jony-java/src/test/java/com/doublejony/stackNQueue/Stack2.java
+++ jony-java/src/test/java/com/doublejony/stackNQueue/Stack2.java
@@ -7,8 +7,9 @@ import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 import static com.doublejony.common.AssertResolve.resolve;
 
@@ -16,16 +17,16 @@ import static com.doublejony.common.AssertResolve.resolve;
  * 프린터
  * <p>
  * 일반적인 프린터는 인쇄 요청이 들어온 순서대로 인쇄합니다. 그렇기 때문에 중요한 문서가 나중에 인쇄될 수 있습니다. 이런 문제를 보완하기 위해 중요도가 높은 문서를 먼저 인쇄하는 프린터를 개발했습니다. 이 새롭게 개발한 프린터는 아래와 같은 방식으로 인쇄 작업을 수행합니다.
- *
+ * <p>
  * 1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
  * 2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
  * 3. 그렇지 않으면 J를 인쇄합니다.
  * 예를 들어, 4개의 문서(A, B, C, D)가 순서대로 인쇄 대기목록에 있고 중요도가 2 1 3 2 라면 C D A B 순으로 인쇄하게 됩니다.
- *
+ * <p>
  * 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 알고 싶습니다. 위의 예에서 C는 1번째로, A는 3번째로 인쇄됩니다.
- *
+ * <p>
  * 현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열 priorities와 내가 인쇄를 요청한 문서가 현재 대기목록의 어떤 위치에 있는지를 알려주는 location이 매개변수로 주어질 때, 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 return 하도록 solution 함수를 작성해주세요.
- *
+ * <p>
  * 제한사항
  * 현재 대기목록에는 1개 이상 100개 이하의 문서가 있습니다.
  * 인쇄 작업의 중요도는 1~9로 표현하며 숫자가 클수록 중요하다는 뜻입니다.
@@ -36,11 +37,11 @@ import static com.doublejony.common.AssertResolve.resolve;
  * [1, 1, 9, 1, 1, 1]	0	5
  * 입출력 예 설명
  * 예제 #1
- *
+ * <p>
  * 문제에 나온 예와 같습니다.
- *
+ * <p>
  * 예제 #2
- *
+ * <p>
  * 6개의 문서(A, B, C, D, E, F)가 인쇄 대기목록에 있고 중요도가 1 1 9 1 1 1 이므로 C D E F A B 순으로 인쇄합니다.
  */
 @RunWith(DataProviderRunner.class)
@@ -49,14 +50,14 @@ public class Stack2 {
     @DataProvider
     public static Object[][] dataProviderAdd() {
         // @formatter:off
-        return new Object[][]{
+        return new Object[][] {
                 {
-                        new int[]{2, 1, 3, 2},
+                        new int[] { 2, 1, 3, 2 },
                         2,
                         1
                 },
                 {
-                        new int[]{1, 1, 9, 1, 1, 1},
+                        new int[] { 1, 1, 9, 1, 1, 1 },
                         0,
                         5
                 }
@@ -66,27 +67,28 @@ public class Stack2 {
 
     @Test
     @UseDataProvider("dataProviderAdd")
-    public void loopApi(int[] priorities, int location, int expected) {
+    public void useQueue(int[] priorities, int location, int expected) {
 
         Stopwatch timer = Stopwatch.createStarted();
 
-        Queue<Integer> queue = Arrays.stream(priorities).boxed().collect(Collectors.toCollection(LinkedList::new));
+        int answer = 1;
+        PriorityQueue<Object> p = new PriorityQueue<>(Collections.reverseOrder());
 
-        while (true) {
-            queue = sortNextJob(queue);
+        Arrays.stream(priorities).forEach(p::add);
+
+        while (!p.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == (int) p.peek()) {
+                    if (i == location) {
+                        p.clear();
+                        break;
+                    }
+                    p.poll();
+                    answer++;
+                }
+            }
         }
-        
-        int answer = 0;
 
         resolve(Thread.currentThread().getStackTrace()[1].getMethodName(), expected, answer, timer.stop());
-    }
-
-    private Queue<Integer> sortNextJob(Queue<Integer> queue) {
-
-        for (int i = 0; i < queue.size(); i++) {
-            
-        }
-
-        return queue;
     }
 }
