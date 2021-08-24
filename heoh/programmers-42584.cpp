b8db5f5c425e6a77f0ddee32227541ee70704bc6
heoh <h0h6h2h5@gmail.com>
2021-08-24 16:16:20 +0000

    포인터로 변경
    미세한 최적화를 시도했지만 이미 컴파일러 최적화 덕에 영향은 없는듯?

--- heoh/programmers-42584.cpp
+++ heoh/programmers-42584.cpp
@@ -14,18 +14,18 @@ vector<int> solution(vector<int> prices) {
     int nPrices = prices.size();
     vector<int> answer(nPrices);
 
-    static Stock stack[MAX_LEN];
-    int top = -1;
-    stack[++top] = { -1, 0 };
+    static Stock stack[MAX_LEN + 1];
+    Stock* top = stack;
+    *(++top) = { -1, 0 };
     prices[nPrices - 1] = 0;
 
-    for (int i = 0; i < nPrices; i++) {
+    for (int i = 0; i < nPrices; ++i) {
         int price = prices[i];
-        while (stack[top].price > price) {
-            Stock& s = stack[top--];
-            answer[s.index] = i - s.index;
+        while (top->price > price) {
+            answer[top->index] = i - top->index;
+            --top;
         }
-        stack[++top] = { i, price };
+        *(++top) = { i, price };
     }
     return answer;
 }
@@ -33,22 +33,22 @@ vector<int> solution(vector<int> prices) {
 /*
 채점을 시작합니다.
 정확성  테스트
-테스트 1 〉	통과 (0.01ms, 3.61MB)
-테스트 2 〉	통과 (0.03ms, 4.24MB)
-테스트 3 〉	통과 (0.14ms, 3.81MB)
-테스트 4 〉	통과 (0.19ms, 4.25MB)
-테스트 5 〉	통과 (0.19ms, 4.3MB)
-테스트 6 〉	통과 (0.01ms, 4.31MB)
-테스트 7 〉	통과 (0.10ms, 3.72MB)
-테스트 8 〉	통과 (0.13ms, 4.3MB)
-테스트 9 〉	통과 (0.02ms, 3.73MB)
-테스트 10 〉	통과 (0.20ms, 3.89MB)
+테스트 1 〉	통과 (0.01ms, 3.76MB)
+테스트 2 〉	통과 (0.03ms, 4.26MB)
+테스트 3 〉	통과 (0.15ms, 4.29MB)
+테스트 4 〉	통과 (0.17ms, 3.8MB)
+테스트 5 〉	통과 (0.21ms, 3.8MB)
+테스트 6 〉	통과 (0.02ms, 4.24MB)
+테스트 7 〉	통과 (0.13ms, 4.18MB)
+테스트 8 〉	통과 (0.12ms, 4.26MB)
+테스트 9 〉	통과 (0.02ms, 4.25MB)
+테스트 10 〉	통과 (0.20ms, 4.26MB)
 효율성  테스트
-테스트 1 〉	통과 (25.70ms, 24.2MB)
-테스트 2 〉	통과 (17.52ms, 18.8MB)
-테스트 3 〉	통과 (27.11ms, 26.8MB)
-테스트 4 〉	통과 (21.77ms, 21.2MB)
-테스트 5 〉	통과 (15.48ms, 16.1MB)
+테스트 1 〉	통과 (26.02ms, 24.2MB)
+테스트 2 〉	통과 (18.94ms, 18.9MB)
+테스트 3 〉	통과 (28.92ms, 26.8MB)
+테스트 4 〉	통과 (21.87ms, 21.3MB)
+테스트 5 〉	통과 (14.07ms, 16.1MB)
 채점 결과
 정확성: 66.7
 효율성: 33.3
