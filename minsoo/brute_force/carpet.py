e386888541666d0d27cfbd837ed3c1a2f1e4a62e
Minsoo Kim <min-soo.kim@outlook.kr>
2021-09-10 01:56:36 +0900

    brute_force/carpet added

---
+++ minsoo/brute_force/carpet.py
@@ -0,0 +1,4 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42842
+
+def solution(brown, yellow):
+    return [(fst := 1 + brown / 4) + (scd := (fst ** 2 - (brown + yellow)) ** 0.5), fst - scd]
