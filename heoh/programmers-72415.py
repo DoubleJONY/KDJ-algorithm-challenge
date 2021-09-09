3626b04dd0fc46151e935c5839e2a9b6bbf14118
heoh <h0h6h2h5@gmail.com>
2021-09-09 10:17:11 +0000

    parse_cards() 구현

--- heoh/programmers-72415.py
+++ heoh/programmers-72415.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 cards = {}
 
 def solution(board, r, c):
@@ -5,3 +8,11 @@ def solution(board, r, c):
     cards = parse_cards(board)
     candidates = make_bitset(cards.keys())
     return find_min_cost(candidates, r, c)
+
+def parse_cards(board):
+    cards = defaultdict(list)
+    for r, row in enumerate(board):
+        for c, shape in enumerate(row):
+            if shape != 0:
+                cards[shape].append((r, c))
+    return cards
