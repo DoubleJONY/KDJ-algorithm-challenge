205fb3df906624d2cc0cc3df6a0cafb1fc14ac3a
heoh <h0h6h2h5@gmail.com>
2021-09-09 10:40:22 +0000

    find_min_cost() 구현

--- heoh/programmers-72415.py
+++ heoh/programmers-72415.py
@@ -1,13 +1,14 @@
 from collections import defaultdict
 
-
+MAX_COST = 0x7fffffff
 cards = {}
 
 def solution(board, r, c):
     global cards
     cards = parse_cards(board)
     candidates = make_bitset(cards.keys())
-    return find_min_cost(candidates, r, c)
+    cur = (r, c)
+    return find_min_cost(candidates, cur)
 
 def parse_cards(board):
     cards = defaultdict(list)
@@ -22,3 +23,35 @@ def make_bitset(shapes):
     for s in shapes:
         b |= 1 << s
     return b
+
+# params
+# - candidates: 남은 모양 (bitset)
+# - r, c: 현재 커서 위치
+# returns
+# - 현재 상황에서 최적으로 처리했을 때의 비용 (최소 이동 횟수)
+def find_min_cost(candidates, cur):
+    if not candidates:
+        return 0
+
+    min_cost = MAX_COST
+    for s in range(1, 6+1):
+        if not bitset_has(candidates, s):
+            continue
+
+        cost_0 = 0
+        cost_0 += flip_card(candidates, cur, cards[s][0])
+        cost_0 += flip_card(candidates, cards[s][0], cards[s][1])
+        bitset_remove(candidates, s)
+        cost_0 += find_min_cost(candidates, cards[s][1])
+        bitset_add(candidates, s)
+
+        cost_1 = 0
+        cost_1 += flip_card(candidates, cur, cards[s][1])
+        cost_1 += flip_card(candidates, cards[s][1], cards[s][0])
+        bitset_remove(candidates, s)
+        cost_1 += find_min_cost(candidates, cards[s][0])
+        bitset_add(candidates, s)
+
+        min_cost = min(cost_0, cost_1)
+
+    return min_cost
