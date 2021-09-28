5cae95eec3b7109c7622950dcd5b4ee2408cf81e
JONY <jony@mail.i-on.net>
2021-09-28 17:48:53 +0900

    Greedy 실패

--- jony-java/src/test/java/com/doublejony/greedy/Greedy2.java
+++ jony-java/src/test/java/com/doublejony/greedy/Greedy2.java
@@ -14,29 +14,32 @@ import java.util.stream.Collectors;
 import static com.doublejony.common.AssertResolve.resolve;
 
 /**
- * 체육복
+ * 조이스틱
  * <p>
- * 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+ * 조이스틱으로 알파벳 이름을 완성하세요. 맨 처음엔 A로만 이루어져 있습니다.
+ * ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA
  *
- * 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+ * 조이스틱을 각 방향으로 움직이면 아래와 같습니다.
  *
- * 제한사항
- * 전체 학생의 수는 2명 이상 30명 이하입니다.
- * 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
- * 여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
- * 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
- * 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+ * ▲ - 다음 알파벳
+ * ▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로)
+ * ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
+ * ▶ - 커서를 오른쪽으로 이동
+ * 예를 들어 아래의 방법으로 "JAZ"를 만들 수 있습니다.
+ *
+ * - 첫 번째 위치에서 조이스틱을 위로 9번 조작하여 J를 완성합니다.
+ * - 조이스틱을 왼쪽으로 1번 조작하여 커서를 마지막 문자 위치로 이동시킵니다.
+ * - 마지막 위치에서 조이스틱을 아래로 1번 조작하여 Z를 완성합니다.
+ * 따라서 11번 이동시켜 "JAZ"를 만들 수 있고, 이때가 최소 이동입니다.
+ * 만들고자 하는 이름 name이 매개변수로 주어질 때, 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
+ *
+ * 제한 사항
+ * name은 알파벳 대문자로만 이루어져 있습니다.
+ * name의 길이는 1 이상 20 이하입니다.
  * 입출력 예
- * n	lost	reserve	return
- * 5	[2, 4]	[1, 3, 5]	5
- * 5	[2, 4]  [3]	        4
- * 3	[3]	    [1]	        2
- * 입출력 예 설명
- * 예제 #1
- * 1번 학생이 2번 학생에게 체육복을 빌려주고, 3번 학생이나 5번 학생이 4번 학생에게 체육복을 빌려주면 학생 5명이 체육수업을 들을 수 있습니다.
- *
- * 예제 #2
- * 3번 학생이 2번 학생이나 4번 학생에게 체육복을 빌려주면 학생 4명이 체육수업을 들을 수 있습니다.
+ * name	return
+ * "JEROEN"	56
+ * "JAN"	23
  */
 @RunWith(DataProviderRunner.class)
 public class Greedy2 {
@@ -46,103 +49,29 @@ public class Greedy2 {
         // @formatter:off
         return new Object[][]{
                 {
-                        5,
-                        new int[]{2, 4},
-                        new int[]{1, 3, 5},
-                        5
+                        "JEROEN",
+                        56
                 },
                 {
-                        5,
-                        new int[]{2, 4},
-                        new int[]{3},
-                        4
-                },
-                {
-                        3,
-                        new int[]{3},
-                        new int[]{1},
-                        2
-                },
-                {
-                        6,
-                        new int[]{2, 3, 5},
-                        new int[]{3, 4},
-                        5
-                },
-                {
-                        6,
-                        new int[]{3},
-                        new int[]{3, 4},
-                        6
-                },
-                {
-                        10,
-                        new int[]{5,4,3,2,1},
-                        new int[]{3,1,2,5,4},
-                        10
+                        "JAN",
+                        23
                 }
         };
         // @formatter:on
     }
 
-    /*
-    테스트 1 〉	통과 (2.15ms, 75MB)
-    테스트 2 〉	통과 (2.76ms, 79MB)
-    테스트 3 〉	통과 (3.37ms, 69.8MB)
-    테스트 4 〉	통과 (2.65ms, 79.2MB)
-    테스트 5 〉	통과 (3.11ms, 76.9MB)
-    테스트 6 〉	통과 (2.07ms, 74.2MB)
-    테스트 7 〉	통과 (3.06ms, 69.6MB)
-    테스트 8 〉	통과 (2.21ms, 76.6MB)
-    테스트 9 〉	통과 (2.94ms, 84.8MB)
-    테스트 10 〉	통과 (2.22ms, 77.2MB)
-    테스트 11 〉	통과 (2.27ms, 77.9MB)
-    테스트 12 〉	통과 (2.05ms, 85.7MB)
-    테스트 13 〉	통과 (2.25ms, 77MB)
-    테스트 14 〉	통과 (2.11ms, 77.7MB)
-    테스트 15 〉	통과 (1.96ms, 74.3MB)
-    테스트 16 〉	통과 (2.08ms, 78.2MB)
-    테스트 17 〉	통과 (2.90ms, 77.6MB)
-    테스트 18 〉	통과 (2.30ms, 71.8MB)
-    테스트 19 〉	통과 (2.66ms, 80.2MB)
-    테스트 20 〉	통과 (2.04ms, 76MB)
-     */
     @Test
     @UseDataProvider("testCase")
-    public void useFullScanLoop(int n, int[] lost, int[] reserve, int expected) {
+    public void useFullScanLoop(String name, int expected) {
 
         Stopwatch timer = Stopwatch.createStarted();
-        resolve(Thread.currentThread().getStackTrace()[1].getMethodName(), expected, new Solution().solution(n, lost, reserve), timer.stop());
+        resolve(Thread.currentThread().getStackTrace()[1].getMethodName(), expected, new Solution().solution(name), timer.stop());
     }
 
     class Solution {
-        public int solution(int n, int[] lost, int[] reserve) {
-            List<Integer> lostList = Arrays.stream(lost).boxed().sorted().collect(Collectors.toList());
-            List<Integer> reserveList = Arrays.stream(reserve).boxed().sorted().collect(Collectors.toList());
-
-            for (int l : lost) {
-                for (int r : reserve) {
-                    if(r == l) {
-                        reserveList.remove((Integer) r);
-                        lostList.remove((Integer) l);
-                        break;
-                    }
-                }
-            }
-
-            int a = n - lostList.size();
-
-            for (int l : lostList) {
-                for (int r : reserveList) {
-                    if(Math.abs(r - l) < 2) {
-                        reserveList.remove((Integer) r);
-                        a++;
-                        break;
-                    }
-                }
-            }
-
-            return a;
+        public int solution(String name) {
+            int answer = 0;
+            return answer;
         }
     }
 }
