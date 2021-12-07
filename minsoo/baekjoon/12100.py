18b78119b644ca533c0060478a45570128238b0d
Minsoo Kim <min-soo.kim@outlook.kr>
2021-12-08 00:28:11 +0900

    baekjoon week 1 2048(Easy)

--- minsoo/baekjoon/12100.py
+++ minsoo/baekjoon/12100.py
@@ -1,5 +1,4 @@
 # https://www.acmicpc.net/problem/12100
-# TBA
 
 
 class Game:
@@ -8,37 +7,44 @@ class Game:
         self.board = [list(map(int, input().split())) for _ in range(self.n)]
 
     def rotate_clockwise(self):
-        self.board = list(map(lambda x: x[::-1], zip(*self.board)))
+        self.board = list(map(lambda x: list(x[::-1]), zip(*self.board)))
 
-    def merge(self):
-        is_merged = False
+    def contract(self):
+        is_contracted = False
         board = []
         for row in self.board:
             stack = []
-            for num in row:
-                while stack and num and num == stack[-1]:
-                    is_merged = True
-                    num *= 2
-                    stack.pop()
-                if num:
-                    stack.append(num)
-            board.append([0] * (self.n - len(stack)) + stack)
+            copied_row = row.copy()
+            while copied_row:
+                num = copied_row.pop()
+                if stack and stack[-1][1] and num == stack[-1][0]:
+                    stack[-1][0] *= 2
+                    stack[-1][1] = False
+                elif num:
+                    stack.append([num, True])
+
+            if stack:
+                stack, _ = zip(*stack[::-1])
+
+            board.append([0] * (self.n - len(stack)) + list(stack))
+            is_contracted |= board[-1] != row
+
         self.board = board
-        return is_merged
+        return is_contracted
 
     def get_max(self):
         return max(max(row) for row in self.board)
 
     def find(self, cnt=0):
-        if cnt > 5:
+        if cnt > 4:
             return self.get_max()
 
         max_num = 0
         for rot in range(4):
             if rot:
                 self.rotate_clockwise()
-            if self.merge():
-                prev_board = self.board.copy()
+            prev_board = self.board.copy()
+            if self.contract():
                 max_num = max(max_num, self.find(cnt + 1))
                 self.board = prev_board
 
@@ -46,8 +52,7 @@ class Game:
 
     @classmethod
     def play(cls):
-        game = cls()
-        return game.find()
+        return cls().find()
 
 
 print(Game.play())
