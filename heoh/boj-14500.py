b48c9de535249426303935bc12c2560d4a6fc7d3
HeoHeo <h0h6h2h5@gmail.com>
2022-01-11 20:09:10 +0900

    테트로미노: 최적화

--- heoh/boj-14500.py
+++ heoh/boj-14500.py
@@ -6,18 +6,25 @@ A = list()
 for _ in range(N):
     A.append(list(map(int, input().split())))
 
+max_in_map = max(max(row) for row in A)
+answer = 0
 
-def get_max_case(y, x, depth, sum):
+
+def get_max_case(y, x, depth, sum, back_chances=1):
     cell = A[y][x]
     if depth == 3:
         return sum+cell
 
+    ideal_sum = sum + max_in_map * (4 - depth)
+    if ideal_sum < answer:
+        return 0
+
     best = 0
-    if depth == 1:
-        A[y][x] = 0
-    else:
-        A[y][x] = -1
-    for d in range(4):
+    A[y][x] = 0
+    dirs = range(3)
+    if depth == 0:
+        dirs = range(2)
+    for d in dirs:
         ny = y + DY[d]
         nx = x + DX[d]
         
@@ -25,20 +32,20 @@ def get_max_case(y, x, depth, sum):
             continue
 
         next_cell = A[ny][nx]
-        if next_cell == -1:
-            continue
-        elif next_cell == 0:
-            best = max(best, get_max_case(ny, nx, depth, sum+cell))
+        if next_cell == 0:
+            if back_chances > 0:
+                best = max(best, get_max_case(ny, nx, depth, sum+cell, back_chances-1))
+            else:
+                continue
         else:
-            best = max(best, get_max_case(ny, nx, depth+1, sum+cell))
+            best = max(best, get_max_case(ny, nx, depth+1, sum+cell, back_chances))
 
     A[y][x] = cell
     return best
 
 
-answer = 0
 for y in range(N):
     for x in range(M):
-        answer = max(answer, get_max_case(y, x, 0, 0))
+        answer = max(answer, get_max_case(y, x, 0, 0, True))
 
 print(answer)
