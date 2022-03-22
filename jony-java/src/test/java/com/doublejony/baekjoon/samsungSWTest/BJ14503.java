53a0faacb5130f345cf4970e95d6dfd64d449915
DoubleJONY <panzz8307@me.com>
2022-03-22 22:14:50 +0900

    로봇청소기
    Still FAIL

--- jony-java/src/test/java/com/doublejony/baekjoon/samsungSWTest/BJ14503.java
+++ jony-java/src/test/java/com/doublejony/baekjoon/samsungSWTest/BJ14503.java
@@ -87,13 +87,13 @@ public class BJ14503 {
 
     public class Main {
 
-        int[] H = new int[]{0, 1, 0, -1, 0};
-        int[] V = new int[]{-1, 0, 1, 0, -1};
+        int[] H = new int[]{0, 1, 0, -1};
+        int[] W = new int[]{-1, 0, 1, 0};
 
         int h;
         int w;
-        int x;
-        int y;
+        int r;
+        int c;
         int direction;
 
         int turnCount = 0;
@@ -105,8 +105,8 @@ public class BJ14503 {
 
             h = Integer.parseInt(input[0].split(" ")[0]);
             w = Integer.parseInt(input[0].split(" ")[1]);
-            x = Integer.parseInt(input[1].split(" ")[0]);
-            y = Integer.parseInt(input[1].split(" ")[1]);
+            r = Integer.parseInt(input[1].split(" ")[0]);
+            c = Integer.parseInt(input[1].split(" ")[1]);
             direction = Integer.parseInt(input[1].split(" ")[2]);
 
             map = new int[h][w];
@@ -124,26 +124,24 @@ public class BJ14503 {
             int answer = 0;
 
             while (true) {
-                if (map[x][y] == 0) {
-                    map[x][y] = 2; //map.put();
+                if (map[r][c] == 0) {
+                    map[r][c] = 2;
                     answer++;
                 }
 
                 if (turnCount >= 4) {
-                    if (map[x - H[direction]][y - V[direction]] == 1) {
+                    if (map[r - H[direction]][c - W[direction]] == 1) {
                         break;
                     } else {
-                        x -= H[direction];
-                        y -= V[direction];
+                        r -= H[direction];
+                        c -= W[direction];
                         turnCount = 0;
                     }
-
-                } else if (map[x + H[direction]][y + V[direction]] == 0) {
-                    x += H[direction];
-                    y += V[direction];
+                } else if (map[r + H[direction]][c + W[direction]] == 0) {
+                    r += H[direction];
+                    c += W[direction];
                     turnLeft();
                     turnCount = 0;
-                    continue;
                 } else {
                     turnLeft();
                     turnCount++;
@@ -154,9 +152,9 @@ public class BJ14503 {
         }
 
         private void turnLeft() {
-            direction++;
-            if (direction > 3) {
-                direction = 0;
+            direction--;
+            if (direction < 0) {
+                direction = 3;
             }
         }
     }
