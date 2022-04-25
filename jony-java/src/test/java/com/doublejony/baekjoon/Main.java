5e2380dbc4a71dc10f38321efc1227f7a649aa3b
DoubleJONY <panzz8307@me.com>
2022-04-25 15:37:02 +0900

    15685: 드래곤 커브

--- jony-java/src/test/java/com/doublejony/baekjoon/Main.java
+++ jony-java/src/test/java/com/doublejony/baekjoon/Main.java
@@ -23,216 +23,80 @@ public class Main {
         System.out.println(answer);
     }
 
-    int N;
-    int M;
-
-    int answer;
+    int[] dx = {1, 0, -1, 0, 1, 0, -1, 0};
+    int[] dy = {0, -1, 0, 1, 0, -1, 0, 1};
 
     public String solution(String[] input) {
-        N = Integer.parseInt(input[0].split(" ")[0]);
-        M = Integer.parseInt(input[0].split(" ")[1]);
-
-        Queue<Camera> cameras = new LinkedList<>();
-
-        answer = 9999999;
-
-        int[][] map = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            String[] row = input[i + 1].split(" ");
-            for (int j = 0; j < M; j++) {
-                if (Integer.parseInt(row[j]) > 0 && Integer.parseInt(row[j]) < 6) {
-                    cameras.add(new Camera(Integer.parseInt(row[j]), i, j));
-                }
-                map[i][j] = Integer.parseInt(row[j]);
-            }
+        int n = Integer.parseInt(input[0]);
+        Queue<Dragon> queue = new LinkedList<>();
+        for (int i = 1; i < input.length; i++) {
+            String[] data = input[i].split(" ");
+            queue.add(new Dragon(Integer.parseInt(data[0]), Integer.parseInt(data[1]), Integer.parseInt(data[2]), Integer.parseInt(data[3])));
         }
 
-        dfs(map, cameras);
+        int map[][] = new int[101][101]; // 100 * 100 이라면서 왜 0 <= x,y <= 100 ?
 
-        return answer == 9999999 ? "0" : String.valueOf(answer);
-    }
-
-    class Camera {
-        int type;
-        int n;
-        int m;
-
-        public Camera(int type, int n, int m) {
-            this.type = type;
-            this.n = n;
-            this.m = m;
+        while (!queue.isEmpty()) {
+            Dragon dragon = queue.poll();
+            draw(map, dragon);
         }
+
+        return String.valueOf(countSquare(map));
     }
 
-    private void dfs(int[][] map, Queue<Camera> cameras) {
-        if (cameras.isEmpty()) {
-            int sum = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (map[i][j] == 0) {
-                        sum++;
-                    }
+    private int countSquare(int[][] map) {
+
+        int answer = 0;
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (map[i][j] == 1 && map[i][j+1] == 1 && map[i+1][j] == 1 && map[i+1][j+1] == 1) {
+                    answer++;
                 }
             }
-
-            answer = Math.min(answer, sum);
-            return;
         }
 
-        int[][] newMap = copyMap(map);
-
-        Camera camera = cameras.poll();
-
-        switch (camera.type) {
-            case 5:
-                fillUp(newMap, camera.n, camera.m);
-                fillDown(newMap, camera.n, camera.m);
-                fillLeft(newMap, camera.n, camera.m);
-                fillRight(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-                return;
-            case 4:
-                // case 1
-                fillLeft(newMap, camera.n, camera.m);
-                fillUp(newMap, camera.n, camera.m);
-                fillRight(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 2
-                newMap = copyMap(map);
-                fillUp(newMap, camera.n, camera.m);
-                fillRight(newMap, camera.n, camera.m);
-                fillDown(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 3
-                newMap = copyMap(map);
-                fillRight(newMap, camera.n, camera.m);
-                fillDown(newMap, camera.n, camera.m);
-                fillLeft(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 4
-                newMap = copyMap(map);
-                fillDown(newMap, camera.n, camera.m);
-                fillLeft(newMap, camera.n, camera.m);
-                fillUp(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                return;
-            case 3:
-                // case 1
-                fillUp(newMap, camera.n, camera.m);
-                fillRight(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 2
-                newMap = copyMap(map);
-                fillRight(newMap, camera.n, camera.m);
-                fillDown(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 3
-                newMap = copyMap(map);
-                fillDown(newMap, camera.n, camera.m);
-                fillLeft(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 4
-                newMap = copyMap(map);
-                fillLeft(newMap, camera.n, camera.m);
-                fillUp(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                return;
-            case 2:
-                // case 1
-                fillLeft(newMap, camera.n, camera.m);
-                fillRight(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 2
-                newMap = copyMap(map);
-                fillUp(newMap, camera.n, camera.m);
-                fillDown(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                return;
-            case 1:
-                // case 1
-                fillRight(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 2
-                newMap = copyMap(map);
-                fillDown(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 3
-                newMap = copyMap(map);
-                fillLeft(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-
-                // case 4
-                newMap = copyMap(map);
-                fillUp(newMap, camera.n, camera.m);
-                dfs(newMap, cloneQueue(cameras));
-        }
+        return answer;
     }
 
-    private Queue<Camera> cloneQueue(Queue<Camera> cameras) {
-        return new LinkedList<>(cameras);
-    }
+    private void draw(int[][] map, Dragon dragon) {
+        int nx = dragon.x;
+        int ny = dragon.y;
 
-    private int[][] copyMap(int[][] map) {
-        int[][] newMap = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            System.arraycopy(map[i], 0, newMap[i], 0, M);
-        }
-        return newMap;
-    }
+        List<Integer> list = new ArrayList<>();
 
-    private void fillUp(int[][] map, int n, int m) {
+        list.add(dragon.d);
 
-        for (int i = n - 1; i >= 0; i--) {
-            if (map[i][m] == 6) {
-                break;
-            } else if (map[i][m] == 0) {
-                map[i][m] = 7;
+        for (int i = 1; i <= dragon.gen; i++) {
+            for (int j = list.size() - 1; j >= 0; j--) {
+                list.add((list.get(j) + 1) % 4);
             }
         }
-    }
 
-    private void fillDown(int[][] map, int n, int m) {
-
-        for (int i = n + 1; i < N; i++) {
-            if (map[i][m] == 6) {
-                break;
-            } else if (map[i][m] == 0) {
-                map[i][m] = 7;
+        map[ny][nx] = 1;
+        for (int a : list) {
+            nx += dx[a];
+            ny += dy[a];
+            if (nx >= 0 && nx <= 100 && ny >= 0 && ny <= 100) {
+                if (map[ny][nx] == 0) {
+                    map[ny][nx] = 1;
+                }
             }
         }
+
     }
 
-    private void fillLeft(int[][] map, int n, int m) {
+    class Dragon {
+        int x;
+        int y;
+        int d;
+        int gen;
 
-        for (int i = m - 1; i >= 0; i--) {
-            if (map[n][i] == 6) {
-                break;
-            } else if (map[n][i] == 0) {
-                map[n][i] = 7;
-            }
-        }
-    }
-
-    private void fillRight(int[][] map, int n, int m) {
-
-        for (int i = m + 1; i < M; i++) {
-            if (map[n][i] == 6) {
-                break;
-            } else if (map[n][i] == 0) {
-                map[n][i] = 7;
-            }
+        public Dragon(int x, int y, int d, int gen) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+            this.gen = gen;
         }
     }
 }
