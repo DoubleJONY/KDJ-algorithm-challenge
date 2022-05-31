556bc82f61fc3809695ca78e6b149fb6c5aa73b7
HeoHeo <h0h6h2h5@gmail.com>
2022-05-31 21:21:35 +0900

    인구 이동: BFS 풀이

--- heoh/boj-16234.py
+++ heoh/boj-16234.py
@@ -4,13 +4,17 @@ from collections import deque
 N, L, R = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
 A = defaultdict(lambda: 1000, {(r, c): A[r][c] for c in range(N) for r in range(N)})
-    
+
 def propagate(p):
     g = set()
     q = deque([p])
     while q:
         p = q.popleft()
         r, c = p
+
+        if p in g:
+            continue
+
         g.add(p)
         for d in range(4):
             nr = r + [0, 1, 0, -1][d]
