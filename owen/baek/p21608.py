3f540f10a1aeee7e8784f0d0346c4c234e4da12b
OWEN-JUN <49712113+OWEN-JUN@users.noreply.github.com>
2022-11-01 20:36:56 +0900

    1101

--- owen/baek/p21608.py
+++ owen/baek/p21608.py
@@ -3,13 +3,12 @@
 n = int(input())
 favorite_friends = [list(map(int, input().split())) for _ in range(n**2)]
 dict_ff = {}
-print(favorite_friends)
+
 for i in favorite_friends:
-    print(i)
+
     dict_ff[i[0]] = i[1:]
 
 
-print(dict_ff)
 # 상하좌우
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
@@ -22,37 +21,41 @@ def run(c_s):
     max_f = -1
     return_x = -1
     return_y = -1
-    for i in range(n-1,-1,-1):
-        for j in range(n-1,-1,-1):
+    tmp_nof_noe = []
+    for i in range(n):
+        for j in range(n):
             tempty = 0
             tfriend = 0
             #4방향 칸 조회
-            for k in range(4):
-                tx, ty = i + dx[k], j + dy[k]
-                if tx >= n or ty >= n or tx < 0 or ty < 0:
-                    continue
-                if class_map[tx][ty] == 0:
-                    tempty += 1
-                elif class_map[tx][ty] in dict_ff[c_s]:
-                    tfriend += 1
+            if class_map[i][j] == 0:
+            
+                for k in range(4):
+                    tx, ty = i + dx[k], j + dy[k]
+                    if tx >= n or ty >= n or tx < 0 or ty < 0:
+                        continue
+                    if class_map[tx][ty] == 0:
+                        tempty += 1
+                    elif class_map[tx][ty] in dict_ff[c_s]:
+                        tfriend += 1
 
-            if max_f <= tfriend:
-                if max_f == tfriend:
-                    #빈칸비교
-                    if max_empty <= tempty:
+                if max_f <= tfriend:
+                    if max_f == tfriend:
+                        #빈칸비교
+                        if max_empty < tempty:
+                            return_x = i
+                            return_y = j
+                            max_empty = tempty
+                            max_f = tfriend
+                    else: # tfriend 가 클때
                         return_x = i
                         return_y = j
                         max_empty = tempty
                         max_f = tfriend
-                else: # tfriend 가 클때
-                    return_x = i
-                    return_y = j
-                    
-                    max_f = tfriend
-
-                    if max_empty <= tempty:
-                        max_empty = tempty
-
+                tmp_nof_noe.append([i,j])
+              
+    if return_x == -1:
+        return_x = tmp_nof_noe[0][0]
+        return_y = tmp_nof_noe[0][1]
 
 
     #결과 좌표
@@ -61,21 +64,23 @@ def run(c_s):
 
 for i in favorite_friends:
     px, py = run(i[0]) # 좌표
+
     class_map[px][py] = i[0]
 
 answer = 0
-print(class_map)
+# print(class_map)
 for i in range(n):
     for j in range(n):
         fcount =0
         for k in range(4):
             tx, ty = i + dx[k], j + dy[k]
-            if tx < n or ty < n or tx >= 0 or ty >= 0:
+            if tx < n and ty < n and tx >= 0 and ty >= 0:
+                
                 if class_map[tx][ty] in dict_ff[class_map[i][j]]:
                     fcount += 1
 
         
-        answer += 10 ** (fcount-1)
+        answer += int(10 ** (fcount-1))
 
 print(answer)
 
