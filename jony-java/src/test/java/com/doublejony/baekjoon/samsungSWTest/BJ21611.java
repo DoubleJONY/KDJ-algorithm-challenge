c225aff8be96058c0562c0ac2e6e6b0341a64f3a
DoubleJONY <jony@i-on.net>
2022-11-22 19:09:47 +0900

    draft

--- jony-java/src/test/java/com/doublejony/baekjoon/samsungSWTest/BJ21611.java
+++ jony-java/src/test/java/com/doublejony/baekjoon/samsungSWTest/BJ21611.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import static com.doublejony.common.AssertResolve.resolve;
@@ -107,6 +108,18 @@ public class BJ21611 {
 
     public class Main {
 
+        int answer1 = 0;
+        int answer2 = 0;
+        int answer3 = 0;
+
+        int dx[] = { 0, -1, 1, 0, 0 }; // 1~4 상하좌우
+        int dy[] = { 0, 0, 0, -1, 1 };
+
+        int nextDir[] = { 0, 3, 4, 2, 1 };
+
+        int[][] indexMap;
+        List<Integer> stringList;
+
         public String solution(String[] input) {
 
             int N = Integer.parseInt(input[0].split(" ")[0]);
@@ -127,9 +140,24 @@ public class BJ21611 {
                 ));
             }
 
-            //map to list
+            prepare(N, map);
+            int center = N / 2;
+
+            int[][] blizzardPoints = new int[4][center];
+            for (int i = 1; i <= 4; i++) {
+                for (int j = 1; j <= center; j++) {
+                    blizzardPoints[i-1][j-1] = indexMap[center + (dx[i] * j)][center + (dy[i] * j)];
+                }
+            }
+
 
             //loop M {
+            removeZero();
+
+            for (int i = 0; i < M; i++) {
+                blizzard(spellQueue.poll(), blizzardPoints);
+                popMarble();
+            }
 
             //use blizzard
 
@@ -146,6 +174,65 @@ public class BJ21611 {
             //}
 
             //return 1*(no1)+2*(no2)+3*(no3)
+
+            return String.valueOf(answer1 + answer2 * 2 + answer3 * 3);
+        }
+
+        private void popMarble() {
+
+            boolean isPoped = false;
+
+            
+
+            removeZero();
+
+            if (isPoped) {
+                popMarble();
+            }
+        }
+
+        private void blizzard(Spell spell, int[][] blizzardPoints) {
+
+            for (int i = 0; i < spell.s; i++) {
+                stringList.set(blizzardPoints[spell.d-1][i] , 0);
+            }
+            removeZero();
+        }
+
+        private void removeZero() {
+            stringList.removeIf(x -> x.equals(0));
+        }
+
+        private void prepare(int N, int[][] map) {
+            int x = N / 2, y = N / 2;
+            int nx = 0, ny = 0;
+            int curDir = 3;
+            int d = 1; // 이동해야하는 양
+            int num = 0;
+
+            indexMap = new int[N][N];
+            indexMap[x][y] = -1;
+
+            stringList = new LinkedList<>();
+
+            while (true) {
+                for (int k = 0; k < 2; k++) {
+                    for (int i = 0; i < d; i++) {
+                        if (x == 0 && y == 0)
+                            return;
+                        nx = x + dx[curDir];
+                        ny = y + dy[curDir];
+                        indexMap[nx][ny] = num;
+                        stringList.add(map[nx][ny]);
+                        num++;
+
+                        x = nx;
+                        y = ny;
+                    }
+                    curDir = nextDir[curDir];
+                }
+                d++;
+            }
         }
 
         private class Spell {
