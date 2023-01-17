7cab4f946c3197b9b351a32ba4c43d33064fc0b3
dktmzdf <dktmzdf@gmail.com>
2023-01-17 11:36:10 +0900

    Modify
    Modify

--- kujin-CSharp/Leet/1114 Print in Order.cs
+++ kujin-CSharp/Leet/1114 Print in Order.cs
@@ -1,79 +1,33 @@
+using System.Threading;
 
+public class Foo {
 
-public class Solution
-{
-    //Definition for singly-linked list.
+    private readonly AutoResetEvent secondARE = new AutoResetEvent(false);
+    private readonly AutoResetEvent thirdARE = new AutoResetEvent(false);
 
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
+    public Foo() {
     
-    public ListNode RotateRight(ListNode head, int k)
-    {
-        ListNode startNode = head;
-        ListNode currentNode = head;
-        ListNode resultNode = null;
-        
+    }
 
-        Stack<ListNode> stack = new Stack<ListNode>();
-        Queue<ListNode> queue = new Queue<ListNode>();
-
-        if (head == null) return null;
+    public void First(Action printFirst) {
         
-        while (currentNode != null)
-        {
-            stack.Push(currentNode);
-            currentNode = currentNode.next;
-        }
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        secondARE.Set();
+    }
 
-        ListNode tmpNode = null;
-        int stackCount = stack.Count;
-        for (int i = 0; i < stackCount; i++)
-        {
-            tmpNode = stack.Pop();
-            tmpNode.next = null;
-            queue.Enqueue(tmpNode);
-        }
+    public void Second(Action printSecond) {
         
-        tmpNode = null;
-        currentNode = null;
-        
-        for (int i = 0; i < k % (queue.Count); i++)
-        {
-            tmpNode = queue.Dequeue();
-            queue.Enqueue(tmpNode);
-        }
-        
-        int queueCount = queue.Count;
-        for (int i = 0; i < queueCount; i++)
-        {
-            stack.Push(queue.Dequeue());
-        }
+        // printSecond() outputs "second". Do not change or remove this line.
+        secondARE.WaitOne();
+        printSecond();
+        thirdARE.Set();
+    }
 
-        if (stack.Count == 0)
-        {
-            return head;
-        }
-        else
-        {
-            resultNode = stack.Pop();
-            currentNode = resultNode;
-            stackCount = stack.Count;
-            for (int i = 0; i < stackCount; i++)
-            {
-                currentNode.next = stack.Pop();
-                currentNode = currentNode.next;
-            }
-
-            return resultNode;
-        }
+    public void Third(Action printThird) {
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        thirdARE.WaitOne();
+        printThird();
     }
 }
