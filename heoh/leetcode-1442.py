e342d6738f603f447562ac825fb086af43544d3a
HeoHeo <h0h6h2h5@gmail.com>
2023-03-13 22:02:03 +0900

    Count Triplets That Can Form Two Arrays of Equal XOR: O(N^2) 최적화

--- heoh/leetcode-1442.py
+++ heoh/leetcode-1442.py
@@ -1,27 +1,14 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
-        self.init_cache(arr)
-        
         n_triplets = 0
         for i in range(len(arr)):
+            value = arr[i]
             for k in range(i+1, len(arr)):
-                if self.xor_range(i, k) == 0:
+                value ^= arr[k]
+                if value == 0:
                     n_triplets += k - i
 
         return n_triplets
-            
-
-    def init_cache(self, arr: List[int]):
-        cache = {}
-        for i in range(len(arr)):
-            value = 0
-            for j in range(i, len(arr)):
-                value ^= arr[j]
-                cache[i, j] = value
-        self.cache = cache
-
-    def xor_range(self, left, right):
-        return self.cache[left, right]
 
 
 def test():
