607782ca761a1bd5ef328306b6ece6bdddb90029
HeoHeo <h0h6h2h5@gmail.com>
2023-03-28 00:29:29 +0900

    Remove All Adjacent Duplicates in String II: 스택 최적화

--- heoh/leetcode-1209.py
+++ heoh/leetcode-1209.py
@@ -1,11 +1,23 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        stack = ''
+        stack = [[None, 0]]
         for c in s:
-            stack += c
-            if stack[-k:] == (c * k):
-                stack = stack[:-k]
-        return stack
+            last_seq = stack[-1]
+            if last_seq[0] == c:
+                last_seq[1] += 1
+            else:
+                last_seq = [c, 1]
+                stack.append(last_seq)
+                continue
+
+            if last_seq[1] == k:
+                stack.pop()
+
+        result = ''
+        for seq in stack[1:]:
+            result += seq[0] * seq[1]
+
+        return result
 
 
 def test_solution():
